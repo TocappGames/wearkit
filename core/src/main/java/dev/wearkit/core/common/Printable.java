06e25b86d09d8d9e23bf7713d769449cea791bbf
Lluis Santos <lluis@ngynx.com>
2020-04-17 00:39:48 +0200

    fix(compatiblity): fixed compatibility with java 7 (removed java 8 code)

--- core/src/main/java/dev/wearkit/core/common/Printable.java
+++ core/src/main/java/dev/wearkit/core/common/Printable.java
@@ -7,13 +7,10 @@ public interface Printable {
 
     Paint DEFAULT_PAINT = new Paint();
 
-    default void print(String text){
-        print(text, 0, 0, DEFAULT_PAINT);
-    }
 
-    default void print(String text, double xPos, double yPos, Paint paint){
-        print(text, xPos, yPos, paint, null);
-    }
+    void print(String text);
+
+    void print(String text, double xPos, double yPos, Paint paint);
 
     void print(String text, double xPos, double yPos, Paint paint, Double angle);
 }
